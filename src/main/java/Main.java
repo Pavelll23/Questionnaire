c94605f58db8e392b879c912630931662a7d75fa
Pavel Kozlov <cozlov.pavel@mail.ru>
2022-12-09 22:49:20 +0300

    Adding code

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+        post.name = "Иван";
+        post.passport = "4444_№_444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7(999)-999-99-99";
+        post.surename = "Иванов";
+        post.subscription = true;
+    }
+}
